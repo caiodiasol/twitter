8aacdfbd4d033b5315fdec1bffde78e759ff62b4
caiodiasol <caiodiasol@proton.me>
2025-10-28 18:56:42 -0300

    config: flake8 formatting adjustments

--- backend/tweets/views.py
+++ backend/tweets/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import status
 from users.models import UserFollowing
 from .models import Tweet, TweetLike, TweetRetweet
 from .serializers import TweetSerializer, TweetCommentSerializer
--- backend/users/views.py
+++ backend/users/views.py
@@ -3,7 +3,6 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.views import TokenObtainPairView
-from django.contrib.auth import authenticate
 from .models import User, UserFollowing
 from .serializers import UserSerializer, UserCreateSerializer
 
