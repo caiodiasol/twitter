cefed46935f11660ebbe17fa5abcc8cf987efaeb
caiodiasol <caiodiasol@proton.me>
2025-10-29 22:42:40 -0300

    fix: add DRF renderer configuration to handle HTML requests

--- backend/twitter/settings.py
+++ backend/twitter/settings.py
@@ -193,6 +193,12 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
 
 # Simple JWT
